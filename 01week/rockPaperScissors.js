97d35ad654c2f6a8c50e2ea3652a89c84e3d93a0
Renata Estes <renataestes@192-168-2-6.2e0c86f1bd15455a9248c63707630c56.plex.direct>
2017-07-17 23:03:58 -0500

    RockPaperScissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -1,5 +1,22 @@
 'use strict';
 
+/**WHITEBOARDING
+If/Else Statements
+Player 1 & 2 start game by shaking fist 3 times and revealing selection on the 4th.
+If Player 1 rock vs rock = ties
+-- Player 1 rock vs paper = loses
+-- Player 1 rock vs scissors = wins
+-- Player 1 paper vs paper = ties
+-- Player 1 paper vs scissors = loses
+-- Player 1 paper vs rock = wins
+etc etc. List out all combos of results.
+...SO...
+Test if Player 1 Wins, Test if Player 2 Wins, Test if it's a tie.
+If (rock) && (rock > scissors)....
+Try Switch statement since you have many options to run through.
+How can you test hand 1 and hand 2 simultaneously? As if in real life?
+**/
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -8,11 +25,26 @@ const rl = readline.createInterface({
 });
 
 
-function rockPaperScissors(hand1, hand2) {
-
-  // Write code here
-
+// MY CODE //
+function rockPaperScissors(hand1, hand2){
+   if (hand1 === 'rock' && hand2 === 'rock') {
+     console.log("It's a tie!");
+   } else if (hand1 === 'paper' && hand2 === 'paper') {
+     console.log("It's a tie!");
+   } else if (hand1 === 'scissors' && hand2 === 'scissors') {
+     console.log("It's a tie!");
+   } else if (hand1 === 'rock' && hand2 === 'rock') {
+     console.log("It's a tie!");
+   } else if (hand1 === 'rock' && hand2 === 'scissors') {
+     console.log("Player 1 wins!");
+   } else if (hand1 === 'rock' && hand2 === 'paper') {
+     console.log("Player 2 wins!");
+   } else if (hand1 === 'paper' && hand2 === 'scissors'){
+     console.log("Player 2 wins!");
+   }
 }
+console.log(rockPaperScissors('rock', 'scissors'));
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
