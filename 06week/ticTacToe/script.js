5cde8d2b35e9d41ac13122dbd4dd3d7266c4f955
Renata Estes <renataestes@Renatas-MacBook-Air.local>
2017-08-15 20:20:51 -0500

    tictactoe2

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,4 +2,35 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  /*
+  We start with a blank tic tac toe board
+  -click on a square(data-cell) to place X or O(event listener)
+  --test to see if anything there first
+  rotates to player
+  -if empty value, place x or o
+  --else "invalid move"
+  -Test for horizontal, vertical, or diagonal win after each turn
+- 4 diff win functions and TicTacToe functions
+const x = 'x'
+const o = 'o'
+
+Identify player x and y = X & O
+callback to row and data-cell from html
+check for tie
+check for win
+reset game
+
+Look up how to get data-cell attributes
+
+else (playreturn=playerturn === X ? O : X)
+put data-cell attributes into an array?
+
+document.querySelector('data-cell')
+.append(playerTurn)
+
+listen for click on RESET button
+--clear data-cells
+
+Define player
+  */
 });
