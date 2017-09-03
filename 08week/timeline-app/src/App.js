f1f4dc5f21defb220c37df9625df4705b7dee26e
Renata Estes <renataestes@Renatas-MacBook-Air.local>
2017-09-03 11:34:58 -0500

    timeline

---
+++ 08week/timeline-app/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Button, ButtonToolbar, Form } from 'react-bootstrap';
+import 'bootstrap/less/bootstrap.less'
+import './App.css';
+import TimeLine from './TimeLine';
+
+
+class App extends Component {
+    /** Two Components, one parent that takes a user's status
+     * and a child that displays all the statuses */
+  constructor(props){
+    super(props);
+    this.state = {
+        inputValue: '',
+        timeLine: [],
+    }
+  }
+  handleChange(e){
+      this.setState({inputValue: e.target.value});
+  }
+  handleClick=()=>{
+      const card = {
+          createdAt: new Date(),
+          text:  this.state.inputValue,
+          color: 'blue',
+          id: this.state.timeLine.length + 1
+      };
+      this.setState({timeLine: [...this.state.timeLine, card], inputValue: ''})
+  };
+  deleteStatus=(id)=>{
+      console.log(id ,this.state.timeLine);
+      const deleteCardList = this.state.timeLine.filter((card, index) =>{
+          return card.id !== id;
+      });
+      this.setState({timeLine: deleteCardList});
+  };
+  render() {
+    return (
+      <div className="App">
+        <input
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.inputValue}
+            type="text"
+        />
+      <Button onClick={this.handleClick}>Submit</Button>
+        <TimeLine deleteStatus={this.deleteStatus} list={this.state.timeLine} />
+      </div>
+    );
+  }
+}
+
+export default App;
